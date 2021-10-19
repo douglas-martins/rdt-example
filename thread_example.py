d3a7debe1f7ae785af6d6d98c01a22e75d3c2127
GSouza <gabriel.souza@suntech.com.br>
2021-10-19 08:25:14 -0300

    execucao default do sender e receiver para a simulacao

--- thread_example.py
+++ thread_example.py
@@ -1,46 +1,76 @@
 import threading
 from threading import Thread
 import time
-from math import ceil
 
-from machine_states import FSM
 from rdt_packet import RdtPacket
 from sender import SenderClass
 from receiver import ReceiverClass
 
 check = threading.Condition()
-alice_state = FSM.STATE_ONE
-bob_state = FSM.STATE_ONE
 packet: RdtPacket
 bob_rcv: RdtPacket
-message = "Mensagem para bob"
+message = "Mensagem para "
+sender = SenderClass(message)
+receiver: ReceiverClass
+
+
+def default_sender():
+    global sender
+    global packet
+    global bob_rcv
+
+    sender.set_rcv_pkt(bob_rcv)
+    packet = sender.finite_machine()
+    packet = sender.finite_machine()
+
+    check.notify()
+    check.release()
+    check.acquire()
+    check.wait()
+
+
+def default_receiver():
+    global packet
+    global bob_rcv
+    global receiver
+
+    check.acquire()
+    check.wait()
+
+    receiver = ReceiverClass(packet)
+    receiver.finite_machine()
+    bob_rcv = receiver.send_ack()
+
+    check.notify()
+    check.release()
 
 
 def alice_routine():
     global packet
     global bob_rcv
-    global message
-    sender = SenderClass(message)
+
     print("Alice will send a packet")
     packet = sender.finite_machine()
 
     check.acquire()
     check.wait()
 
-    for a in range(0, ceil(len(message)/5)):
-        sender.set_rcv_pkt(bob_rcv)
-        packet = sender.finite_machine()
-        packet = sender.finite_machine()
+    default_sender()
 
-        check.notify()
-        check.release()
-        check.acquire()
-        check.wait()
+    default_sender()
+
+    default_sender()
+
+    default_sender()
+
+    default_sender()
 
 
 def bob_routine():
     global packet
     global bob_rcv
+    global receiver
+
     check.acquire()
 
     receiver = ReceiverClass(packet)
@@ -49,17 +79,12 @@ def bob_routine():
 
     check.notify()
     check.release()
-    print(ceil(len(message) / 5))
-    for a in range(0, ceil(len(message) / 5)):
-        check.acquire()
-        check.wait()
 
-        receiver = ReceiverClass(packet)
-        receiver.finite_machine()
-        bob_rcv = receiver.send_ack()
-
-        check.notify()
-        check.release()
+    default_receiver()
+    default_receiver()
+    default_receiver()
+    default_receiver()
+    default_receiver()
 
 
 Thread(target=alice_routine).start()
